799739d446d1415301b100b9c76efefa5b9ea942
arham-ali1323 <arham.ali1323@gmail.com>
2026-01-02 21:31:12 +0500

    awais Portfoilio update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import ContactUs from './components/ContactUs/page';
-// import PortfolioGrid from './components/PortfolioGrid/page';
-import PortfolioHero from './components/PortfolioHero/page';
-import WorkPortfoilio from './components/WorkPortfoilio/page';
-import Biography from './components/BioGraphy/page';
+import ContactUs from './components/ContactUs/page.jsx';
+// import PortfolioGrid from './components/PortfolioGrid/page.jsx';
+import PortfolioHero from './components/PortfolioHero/page.jsx';
+import WorkPortfoilio from './components/WorkPortfoilio/page.jsx';
+import Biography from './components/BioGraphy/page.jsx';
 import './index.css'
 import './App.css'
 
--- src/components/PortfolioHero/Page.jsx
+++ src/components/PortfolioHero/Page.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import AwaisImage from "../../assets/awais-1.jpeg";
 import data from "../../Data/data.json";
 
